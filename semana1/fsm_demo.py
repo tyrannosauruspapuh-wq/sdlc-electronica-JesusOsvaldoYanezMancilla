017495e3b803b54a7b3c479f6c72c10da718e8dc
Jesús O. <tyrannosauruspapuh@gmail.com>
2026-07-15 18:59:24 -0600

    Se agrega el código demo
    Se agrega el código correspondiente a la demo de la máquina de estados finita

---
+++ semana1/fsm_demo.py
@@ -0,0 +1,24 @@
+from enum import Enum, auto
+ 
+class TrafficLightState(Enum):
+    RED = auto(); YELLOW = auto(); GREEN = auto()
+ 
+class TrafficLightFSM:
+    """El estado vive dentro del objeto, no en una variable global."""
+    def __init__(self) -> None:
+        self._state = TrafficLightState.RED
+        self._cycle_count = 0
+ 
+    @property
+    def state(self) -> TrafficLightState:
+        return self._state
+ 
+    def transition(self) -> TrafficLightState:
+        transitions = {
+            TrafficLightState.RED: TrafficLightState.GREEN,
+            TrafficLightState.GREEN: TrafficLightState.YELLOW,
+            TrafficLightState.YELLOW: TrafficLightState.RED,
+        }
+        self._state = transitions[self._state]
+        self._cycle_count += 1
+        return self._state
