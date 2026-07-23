494fd1b3300a2fb34529398e531822b7f4babcb8
Jesús O. <tyrannosauruspapuh@gmail.com>
2026-07-23 13:15:44 -0600

    Creacion de sensor_registry - Jesús
    Se agrega el código para pasar la mínima del test

---
+++ semana2/sensor_registry.py
@@ -0,0 +1,15 @@
+class SensorNotFoundError(Exception):
+    """Excepción cuando un sensor no existe en el registro."""
+
+    pass
+
+
+class SensorRegistry:
+
+    def __init__(self) -> None:
+        self._sensors: dict[str, dict] = {}
+
+    def get(self, sensor_id: str) -> dict:
+        if sensor_id not in self._sensors:
+            raise SensorNotFoundError(f"El sensor '{sensor_id}' no existe.")
+        return self._sensors[sensor_id]
