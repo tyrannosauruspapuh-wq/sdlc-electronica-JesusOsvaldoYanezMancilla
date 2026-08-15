d98af88e0039873445cb785133a804f5e1462dcc
Jesús O. <tyrannosauruspapuh@gmail.com>
2026-08-14 19:39:51 -0600

    Se modifica semana5/conversions.py. - Jesús

--- semana5/conversions.py
+++ semana5/conversions.py
@@ -1,3 +1,8 @@
 def celsius_to_fahrenheit(c: float) -> float:
     """Convierte grados Celsius a Fahrenheit."""
     return round((c * 9 / 5) + 32, 2)
+
+
+def fahrenheit_to_celsius(f: float) -> float:
+    """Convierte grados Fahrenheit a Celsius y redondea a 2 decimales."""
+    return round((f - 32) * 5 / 9, 2)
